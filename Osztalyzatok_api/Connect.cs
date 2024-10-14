b0602b8b99c4cab3111c6575816f0f89dede0f67
MrTahil <hiloczkit@kkszki.hu>
2024-10-14 08:29:56 +0200

    Connect file creation

---
+++ Osztalyzatok_api/Connect.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+namespace Osztalyzatok_api
+{
+    public class Connect
+    {
+        public MySqlConnection Connection;
+        private string MysqlConnectionString;
+
+        private string Host;
+        private string Database;
+        private string User;
+        private string Password;
+
+        public Connect()
+        {
+            Host = "localhost";
+            Database = "jegyek";
+            User = "root";
+            Password = "";
+            MysqlConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + User + ";PASSWORD=" + Password + ";SslMode=None";
+            Connection = new MySqlConnection(MysqlConnectionString);
+        }
+    }
+}
